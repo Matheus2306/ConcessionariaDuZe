670d067e56d0129929e0f31e5cfc70d38b9d200f
matheus2306 <math2306.r@gmail.com>
2025-02-12 08:53:17 -0300

    Criação de model Veiculos

--- ConcessionariaDuZe/Model/Veiculos.cs
+++ ConcessionariaDuZe/Model/Veiculos.cs
@@ -4,10 +4,14 @@ namespace ConcessionariaDuZe.Model
 {
     public class Veiculos
     {
-        public Guid VeiculosId { get; set; }
+        public Guid VeiculoId { get; set; }
         public string Marca { get; set; }
         public string Modelo { get; set; }
         public string Ano { get; set; }
         public decimal Valor { get; set; }
+        public decimal Quilometragem { get; set; }
+        public string TipoDeCombustivel { get; set; }
+        public string Transmissao { get; set; }
+        public int Estoque { get; set; }
     }
 }
