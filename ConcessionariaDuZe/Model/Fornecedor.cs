e34e5d0db48c1cba0ab6ba9a64437616b2d8aa57
matheus2306 <math2306.r@gmail.com>
2025-02-12 11:47:53 -0300

    Model de fornecedor

---
+++ ConcessionariaDuZe/Model/Fornecedor.cs
@@ -0,0 +1,11 @@
+﻿namespace ConcessionariaDuZe.Model
+{
+    public class Fornecedor
+    {
+        public Guid FornecedorId { get; set; }
+        public string NomeFornecedor { get; set; }
+        public string CNPJ { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
